409d08e8e46c07cda446b3482f4f68453cc75444
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2025-05-31 21:28:49 +0700

    installable (manifest.json)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo/gp_512.png", "logo/**"],
+      manifest: {
+        name: "GoalPocket: Perencana dan Analisis Keuangan",
+        short_name: "GoalPocket",
+        description: "Aplikasi berbasis web untuk mencatat transaksi keuangan, memvisualisasikan data, dan memprediksi tabungan secara cerdas.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0f172a",
+        icons: [
+          {
+            src: "/logo/gp_512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
